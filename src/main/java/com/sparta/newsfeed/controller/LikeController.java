a8cfa33c82bf24dc199be15df3f3c7bfc4e82bc8
ironshine <cjfgml2320@gmail.com>
2024-09-03 22:40:12 +0900

    @cheolhee Add : Like 다건 조회 기능 추가, 좋아요 추가 @GetMapping -> @PostMapping 변경, service 간결화 및 좋아요 유무확인하기 추가

--- src/main/java/com/sparta/newsfeed/controller/LikeController.java
+++ src/main/java/com/sparta/newsfeed/controller/LikeController.java
@@ -1,10 +1,11 @@
 package com.sparta.newsfeed.controller;
 
-import com.sparta.newsfeed.dto.like.LikeRequestDto;
 import com.sparta.newsfeed.dto.like.LikeResponseDto;
 import com.sparta.newsfeed.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,7 +13,7 @@ public class LikeController {
 
     private final LikeService likeService;
 
-    @GetMapping("/api/likes") // 좋아요 추가
+    @PostMapping("/api/likes") // 좋아요 추가
     public LikeResponseDto addLike(
             @RequestParam("userId") Long userId,
             @RequestParam("type") Boolean type,
@@ -26,5 +27,12 @@ public class LikeController {
             @RequestParam("type") Boolean type,
             @RequestParam("itemId") Long itemId) {
         likeService.deleteLike(userId, type, itemId);
+    }
+
+    @GetMapping("/api/likes") // 좋아요 다건 조회
+    public List<LikeResponseDto> getLikes(
+            @RequestParam("userId") Long userId,
+            @RequestParam(name = "type", required = false) Boolean type) {
+        return likeService.getLikes(userId, type);
     }
 }
--- src/main/java/com/sparta/newsfeed/service/LikeService.java
+++ src/main/java/com/sparta/newsfeed/service/LikeService.java
@@ -1,6 +1,5 @@
 package com.sparta.newsfeed.service;
 
-import com.sparta.newsfeed.dto.like.LikeRequestDto;
 import com.sparta.newsfeed.dto.like.LikeResponseDto;
 import com.sparta.newsfeed.entity.User;
 import com.sparta.newsfeed.entity.like.Like;
@@ -10,6 +9,8 @@ import com.sparta.newsfeed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,21 +23,46 @@ public class LikeService {
     @Transactional
     public LikeResponseDto addLike(Long userId, Boolean type, Long itemId) {
         // 유저 존재 확인
-        User user = userRepository.findById(userId).orElseThrow(() -> new NullPointerException("없는 유저ID입니다."));
+        User user = findUserId(userId);
         // 좋아요 생성
         Like like = new Like(type, itemId, user);
+        // 이미 좋아요있는지 확인
+        if (findTypeItemId(type, itemId).getUser().getId().equals(userId)) {
+            throw new IllegalArgumentException("이미 좋아요를 눌렀습니다.");
+        }
         likeRepository.save(like);
 
-        return new LikeResponseDto(like, userId);
+        return new LikeResponseDto(like);
     }
 
     @Transactional
     public void deleteLike(Long userId, Boolean type, Long itemId) {
         // 유저 존재 확인
-        User user = userRepository.findById(userId).orElseThrow(() -> new NullPointerException("없는 유저ID입니다."));
+        User user = findUserId(userId);
         // 좋아요 삭제
-//        likeRepository.delete(likeRepository.findByTypeAndItemIdIn(
-//                type ? LikeTypeEnum.Type.POST : LikeTypeEnum.Type.COMMENT,
-//                itemId));
+        likeRepository.delete(findTypeItemId(type, itemId));
+    }
+
+    public List<LikeResponseDto> getLikes(Long userId, Boolean type) {
+        List<Like> likeList;
+        if (type == null) {
+            // type null 일때 전체 조회
+            likeList = likeRepository.findAllByUserOrderByCreateAtDesc(findUserId(userId));
+        } else {
+            // type 에 따라 post/comment 다건 조회
+            likeList = likeRepository.findAllByUserAndTypeOrderByCreateAtDesc(findUserId(userId), type ? LikeTypeEnum.POST : LikeTypeEnum.COMMENT);
+        }
+        return likeList.stream().map(LikeResponseDto::new).toList();
+    }
+
+
+    // type, itemId 로 Like 가져오기 메서드
+    public Like findTypeItemId(Boolean type, Long itemId) {
+        return likeRepository.findByTypeAndItemId(type ? LikeTypeEnum.POST : LikeTypeEnum.COMMENT, itemId);
+    }
+
+    // userId로 User 가져오기 메서드
+    public User findUserId(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new NullPointerException("없는 유저ID입니다."));
     }
 }
