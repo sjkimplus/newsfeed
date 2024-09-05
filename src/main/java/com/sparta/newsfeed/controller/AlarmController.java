1d1d2ed0b66ebc64937f1bfcd60237504f4695c7
ironshine <cjfgml2320@gmail.com>
2024-09-05 13:41:41 +0900

    @cheolhee Refact : ResponseEntity 추가

--- src/main/java/com/sparta/newsfeed/controller/AlarmController.java
+++ src/main/java/com/sparta/newsfeed/controller/AlarmController.java
@@ -7,6 +7,7 @@ import com.sparta.newsfeed.dto.alarm.AlarmTextResponseDto;
 import com.sparta.newsfeed.entity.alarm.AlarmTypeEnum;
 import com.sparta.newsfeed.service.AlarmService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,20 +19,21 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     @PostMapping("/api/alarms") // 알림 추가
-    public AlarmResponseDto addAlarms(
+    public ResponseEntity<AlarmResponseDto> addAlarms(
             @Auth AuthUser authUser,
             @RequestParam("type") AlarmTypeEnum type,
             @RequestParam("itemId") Long itemId) {
-        return alarmService.addAlarms(authUser.getEmail(), type, itemId);
+        return ResponseEntity.ok(alarmService.addAlarms(authUser.getEmail(), type, itemId));
     }
 
     @GetMapping("/api/alarms") // 알림 다건 조회
-    public List<AlarmTextResponseDto> getAlarms(@Auth AuthUser authUser) {
-        return alarmService.getAlarms(authUser.getEmail());
+    public ResponseEntity<List<AlarmTextResponseDto>> getAlarms(@Auth AuthUser authUser) {
+        return ResponseEntity.ok(alarmService.getAlarms(authUser.getEmail()));
     }
 
     @DeleteMapping("/api/alarms") // 알림 삭제
-    public void deleteAlarm(@Auth AuthUser authUser, @RequestParam("alarmId") Long alarmId) {
+    public String deleteAlarm(@Auth AuthUser authUser, @RequestParam("alarmId") Long alarmId) {
         alarmService.deleteAlarm(authUser.getEmail(), alarmId);
+        return "알림 삭제 완료";
     }
 }
